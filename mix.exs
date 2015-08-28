1f95ff291e8958d0fde5b780ab82d018efa01c97
imae <m-imae@biglobe.co.jp>
2015-08-29 00:21:33 +0900

    change elixir version 1.1-dev to 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gil.Mixfile do
   def project do
     [app: :gil,
      version: "0.0.1",
-     elixir: "~> 1.1-dev",
+     elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      description: description,
